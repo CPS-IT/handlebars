9995f34d1c73fb81a97d894fa698c39bd14083fc
Elias Häußler <e.haeussler@familie-redlich.de>
2022-03-23 18:54:44 +0100

    [TASK] Ignore VCS-ignored files when running PHP-CS-Fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,5 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->getFinder()->in(__DIR__);
+$finder = $config->getFinder()
+    ->in(__DIR__)
+    ->ignoreVCSIgnored(true);
+
 return $config;

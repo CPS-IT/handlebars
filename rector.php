33dcce3dd20915c7672bf790b087d1c6abf29526
Elias Häußler <e.haeussler@familie-redlich.de>
2024-04-02 08:55:25 +0200

    [TASK] Run PHP-CS-Fixer

--- rector.php
+++ rector.php
@@ -49,7 +49,7 @@ return static function (RectorConfig $rectorConfig): void {
     // If you only want to process one/some TYPO3 extension(s), you can specify its path(s) here.
     // If you use the option --config change __DIR__ to getcwd()
     $rectorConfig->paths([
-       __DIR__,
+        __DIR__,
     ]);
 
     // If you use importNames(), you should consider excluding some TYPO3 files.

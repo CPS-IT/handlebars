c3d7d6d8fa6a9a881a90172ef9459ac412d88a11
Elias Häußler <e.haeussler@familie-redlich.de>
2024-09-18 07:56:15 +0200

    [TASK] Simplify Rector configuration

--- rector.php
+++ rector.php
@@ -22,52 +22,22 @@ declare(strict_types=1);
  */
 
 use Rector\Config\RectorConfig;
-use Rector\PostRector\Rector\NameImportingPostRector;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\ValueObject\PhpVersion;
-use Ssch\TYPO3Rector\CodeQuality\General\ConvertImplicitVariablesToExplicitGlobalsRector;
-use Ssch\TYPO3Rector\CodeQuality\General\ExtEmConfRector;
-use Ssch\TYPO3Rector\Configuration\Typo3Option;
 use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        Typo3LevelSetList::UP_TO_TYPO3_10,
-    ]);
-
-    // In order to have a better analysis from phpstan we teach it here some more things
-    $rectorConfig->phpstanConfig(Typo3Option::PHPSTAN_FOR_RECTOR_PATH);
-
-    // Disable parallel otherwise non php file processing is not working i.e. typoscript
-    $rectorConfig->disableParallel();
-
-    // this will not import root namespace classes, like \DateTime or \Exception
-    $rectorConfig->importShortClasses(false);
-
-    // Define your target version which you want to support
-    $rectorConfig->phpVersion(PhpVersion::PHP_71);
-
-    // If you only want to process one/some TYPO3 extension(s), you can specify its path(s) here.
-    // If you use the option --config change __DIR__ to getcwd()
     $rectorConfig->paths([
-        __DIR__,
+        __DIR__ . '/Classes',
+        __DIR__ . '/Configuration',
+        __DIR__ . '/Tests',
+        __DIR__ . '/ext_*.php',
     ]);
 
-    // If you use importNames(), you should consider excluding some TYPO3 files.
-    $rectorConfig->skip([
-        __DIR__ . '/.Build/*',
-        __DIR__ . '/.github/*',
-        __DIR__ . '/config/*',
-        __DIR__ . '/Resources/Private/Libs/Build/vendor/*',
-        __DIR__ . '/var/*',
-        NameImportingPostRector::class => [
-            __DIR__ . '/ext_*.php',
-            __DIR__ . '/Configuration/Services.php',
-        ],
-    ]);
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
 
-    // Add some general TYPO3 rules
-    $rectorConfig->rule(ConvertImplicitVariablesToExplicitGlobalsRector::class);
-    $rectorConfig->ruleWithConfiguration(ExtEmConfRector::class, [
-        ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        Typo3LevelSetList::UP_TO_TYPO3_12,
     ]);
 };

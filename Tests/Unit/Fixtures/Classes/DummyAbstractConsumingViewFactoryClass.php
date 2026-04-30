8483b42aafc56b5ca40c064242f9acda62c7d320
Elias Häußler <e.haeussler@familie-redlich.de>
2026-04-30 09:07:45 +0200

    [BUGFIX] Limit view factory override implementation to certain services

---
+++ Tests/Unit/Fixtures/Classes/DummyAbstractConsumingViewFactoryClass.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS extension "handlebars".
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace CPSIT\Typo3Handlebars\Tests\Unit\Fixtures\Classes;
+
+/**
+ * DummyConsumingViewFactoryClass
+ *
+ * @author Elias Häußler <e.haeussler@familie-redlich.de>
+ * @license GPL-2.0-or-later
+ * @internal
+ */
+abstract class DummyAbstractConsumingViewFactoryClass
+{
+    public function __construct(
+        public ?View\ViewFactory $viewFactory = null,
+    ) {}
+
+    public function setViewFactory(View\ViewFactory $viewFactory): void
+    {
+        $this->viewFactory = $viewFactory;
+    }
+}
